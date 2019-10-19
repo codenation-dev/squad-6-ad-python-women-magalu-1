ec029c7c455a8ebb3102cd0f924fcb090c2ac9ec
Tamirys Pino <taamypino@gmail.com>
2019-10-19 18:47:07 -0300

    Merging model Origin by Thais (#3)
    * origin
    
    * .gitignore
    
    * remoção de arquivo
    
    * ajuste .gitignore

--- central-erros/errorcenter/api/models.py
+++ central-erros/errorcenter/api/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Origin(models.Model):
+    description = models.TextField("Descrição", max_length=500)
--- central-erros/errorcenter/api/views.py
+++ central-erros/errorcenter/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import OriginSerializer
+from .models  import Origin
+
+class OriginApiViewSet(viewsets.ModelViewSet):
+    queryset = Origin.objects.all()
+    serializer_class = OriginSerializer
