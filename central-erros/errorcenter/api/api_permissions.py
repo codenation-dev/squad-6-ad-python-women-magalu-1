7227f1d92553b290d1a90a6243a21bece87be8e9
talitavaleria <talita.v.matta@gmail.com>
2019-11-03 23:01:37 -0300

    Removendo permissão não utilizada

--- central-erros/errorcenter/api/api_permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class OnlyAdminCanList(BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'GET':
-            return request.user.is_staff
-        return True
